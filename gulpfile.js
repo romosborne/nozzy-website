8ef0d848c5fd16734eb98e373be421a8ca70c538
Ozzy <romosborne@gmail.com>
2018-04-17 22:52:49 +0100

    hi ozzy i am a real programmer - editing ur websites

--- gulpfile.js
+++ gulpfile.js
@@ -130,7 +130,7 @@ gulp.task('browserSync', function() {
 });
 
 // Dev task
-gulp.task('dev', ['css', 'js', 'browserSync'], function() {
+gulp.task('dev', ['css', 'js', 'img', 'browserSync'], function() {
     gulp.watch('./scss/*.scss', ['css']);
     gulp.watch('./js/*.js', ['js']);
     gulp.watch('./*.html', browserSync.reload);

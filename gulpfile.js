6c8b0cc3fd821fde003eae5c7786f52ca6dcca7c
Richard Osborne <romosborne@gmail.com>
2019-03-05 23:12:57 +0000

    update site with new images & video, remove legacy imgs

--- gulpfile.js
+++ gulpfile.js
@@ -103,9 +103,9 @@ function img() {
     return gulp.src('img-src/bg.jpg')
         .pipe(responsive({
             'bg.jpg': [
-                {width: '20%', rename: {suffix: '@1x'}},
-                {width: '40%', rename: {suffix: '@2x'}},
-                {width: '60%', rename: {suffix: '@3x'}},
+                // {width: '20%', rename: {suffix: '@1x'}},
+                // {width: '40%', rename: {suffix: '@2x'}},
+                // {width: '60%', rename: {suffix: '@3x'}},
                 {width: '80%', rename: {suffix: '@4x'}},
                 {width: '100%', rename: {suffix: '@5x'}}
             ]
@@ -148,6 +148,7 @@ function browserSyncReload(done) {
 function watchFiles() {
   gulp.watch("./scss/**/*", css);
   gulp.watch(["./js/**/*.js", "!./js/*.min.js"], js);
+  gulp.watch("./img-src/*", img);
   gulp.watch("./**/*.html", browserSyncReload);
 }
 
